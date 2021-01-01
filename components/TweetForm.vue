fb9d77e2e487cddfa6024f2bc264bee74306e8ce
flomero <flo@fridaysforfuture.email>
2021-01-01 21:21:41 +0100

    🚧  tweet form

--- components/TweetForm.vue
+++ components/TweetForm.vue
@@ -1,35 +1,42 @@
 <template>
-    <form V-on:submit.prevent="submitForm">
-      <b-field label="Name" label-position="on-border">
-        <b-input 
-          v-model="tweetName"
-        ></b-input>
-      </b-field>
-      <b-field label="Tweet Text" label-position="on-border">
-        <b-input
-          v-model="tweetText"
-          type="textarea"
-          maxlength="280"
-        ></b-input>
-      </b-field>
-      <b-field>
-        <b-button>New Tweet</b-button>
-      </b-field>
-      </form>
+  <form>
+    <b-field label="Name" label-position="on-border">
+      <b-input v-model="tweetName"></b-input>
+    </b-field>
+    <b-field label="Tweet Text" label-position="on-border">
+      <b-input v-model="tweetText" type="textarea" maxlength="280"></b-input>
+    </b-field>
+    <b-field>
+      <b-button @click.prevent="submitForm">New Tweet</b-button>
+    </b-field>
+  </form>
 </template>
 
 <script>
 export default {
-   data() {
+  data() {
     return {
       tweetName: '',
-      tweetText: ''
+      tweetText: '',
     }
   },
   methods: {
     submitForm() {
-      console.log("baum")
-    }
-  }
+      /* hier uuid generieren */
+      function uuidv4() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+          /[xy]/g,
+          function (c) {
+            var r = (Math.random() * 16) | 0,
+              v = c == 'x' ? r : (r & 0x3) | 0x8
+            return v.toString(16)
+          }
+        )
+      }
+      const id = uuidv4()
+      this.id = id
+      console.log(this.id + this.tweetName + this.tweetText)
+    },
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="section">
-  <div class="container">
-    <div class="columns is-centered is-mobile">
-      <div class="column is-half">
-      <h1 class="title">Volery - tweet together!</h1>
-      <TweetForm />
-  </div>
-  </div>
-  </div>
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-half">
+          <h1 class="title">Volery - tweet together!</h1>
+          <TweetForm />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
