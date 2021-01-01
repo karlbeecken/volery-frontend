14d964da76fc388c83ca06c277a22feffc226cb7
flomero <flo@fridaysforfuture.email>
2021-01-01 19:45:32 +0100

    🚧  tweet Form

---
+++ components/TweetForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <form V-on:submit.prevent="submitForm">
+      <b-field label="Name" label-position="on-border">
+        <b-input 
+          v-model="tweetName"
+        ></b-input>
+      </b-field>
+      <b-field label="Tweet Text" label-position="on-border">
+        <b-input
+          v-model="tweetText"
+          type="textarea"
+          maxlength="280"
+        ></b-input>
+      </b-field>
+      <b-field>
+        <b-button>New Tweet</b-button>
+      </b-field>
+      </form>
+</template>
+
+<script>
+export default {
+   data() {
+    return {
+      tweetName: '',
+      tweetText: ''
+    }
+  },
+  methods: {
+    submitForm() {
+      console.log("baum")
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,50 +1,18 @@
 <template>
+  <div class="section">
   <div class="container">
-    <div>
-      <h1 class="title">Volery</h1>
-      <b-button type="is-primary" @click="this.submitTweet">New Tweet</b-button>
-      <TweetEditor v-on:event="this.updateTweetText" />
-    </div>
+    <div class="columns is-centered is-mobile">
+      <div class="column is-half">
+      <h1 class="title">Volery - tweet together!</h1>
+      <TweetForm />
+  </div>
+  </div>
+  </div>
   </div>
 </template>
 
 <script>
-import TweetEditor from '@/components/TweetEditor.vue'
-import Axios from 'axios'
 export default {
-  data() {
-    return {
-      tweet: '',
-    }
-  },
-  name: 'App',
-  components: {
-    TweetEditor,
-  },
-  methods: {
-    async fetchSomething() {
-      const ip = await this.$axios.$get('http://icanhazip.com')
-      this.ip = ip
-    },
-    updateTweetText(value) {
-      this.tweet = value
-    },
-    async submitTweet(e) {
-      console.log(this.tweet)
-      await this.fetchSomething()
-      console.log(this.ip)
-    },
-  },
+
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-</style>
