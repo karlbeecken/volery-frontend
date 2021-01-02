659177efd07f55fab187be7c977817ff5c92c08a
flomero <flo@fridaysforfuture.email>
2021-01-02 13:19:23 +0100

    ➕ vee-validate

--- components/TweetForm.vue
+++ components/TweetForm.vue
@@ -21,22 +21,7 @@ export default {
     }
   },
   methods: {
-    submitForm() {
-      /* hier uuid generieren */
-      function uuidv4() {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-          /[xy]/g,
-          function (c) {
-            var r = (Math.random() * 16) | 0,
-              v = c == 'x' ? r : (r & 0x3) | 0x8
-            return v.toString(16)
-          }
-        )
-      }
-      const id = uuidv4()
-      this.id = id
-      console.log(this.id + this.tweetName + this.tweetText)
-    },
+    submitForm() {},
   },
 }
 </script>
