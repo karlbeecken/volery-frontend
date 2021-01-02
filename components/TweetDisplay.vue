d0a789e7d061465fc423337ce2166ca2abc5e126
flomero <florianml.fischer@fau.de>
2021-01-02 19:22:32 +0100

    ✨  added tweet slug

---
+++ components/TweetDisplay.vue
@@ -0,0 +1,14 @@
+<template>
+  <section>
+    <h1 class="title">{{ tweet.title }}</h1>
+    <p>{{ tweet.body }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['tweet'],
+}
+</script>
+
+<style></style>
--- pages/v/_tweet.vue
+++ pages/v/_tweet.vue
@@ -1,9 +1,24 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-half"></div>
+        <TweetDisplay :tweet="tweetData" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ params, $axios }) {
+    const tweet = params.tweet
+    const tweetData = await $axios.$get(
+      'https://jsonplaceholder.typicode.com/posts/1'
+    )
+    return { tweetData }
+  },
+}
 </script>
 
 <style></style>
