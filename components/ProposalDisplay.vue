8d78c9257e258732059a25e4777631d783461c36
flomero <florianml.fischer@fau.de>
2021-01-23 21:49:13 +0100

    🐛  newline bug

--- components/ProposalDisplay.vue
+++ components/ProposalDisplay.vue
@@ -2,7 +2,10 @@
   <div>
     <div v-for="prop in tweet.proposals.slice().reverse()" :key="prop.date">
       <div class="box mt-3">
-        <p v-html="$options.filters.highlightText(prop.text)"></p>
+        <p
+          v-html="$options.filters.highlightText(prop.text)"
+          style="white-space: pre"
+        ></p>
         <b-tag class="mt-1">{{ prop.date | formatDate }}</b-tag>
       </div>
     </div>
